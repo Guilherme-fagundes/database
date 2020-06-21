1156a5247d7627eca282520d159081e1f68ceb70
Guilherme-fagundes <guilhermekonflanz94@gmail.com>
2020-06-21 01:58:43 -0300

    [UPD] add gitignore

--- test/index.php
+++ test/index.php
@@ -1,1 +1,13 @@
 <?php
+require __DIR__ . './../vendor/autoload.php';
+require __DIR__.'./../app/config.php';
+
+$read = new \database\Read();
+
+
+$read->find('adm_user', 'WHERE adm_user_id != :id', "id=1");
+
+
+var_dump($read->getResult());
+
+
