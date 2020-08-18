eafcc863d3a83e2fa9028cb1488a4ffc442237cf
Guilherme-fagundes <guilhermekonflanz94@gmail.com>
2020-08-18 01:36:47 -0300

    [UPD] Add pagenator

--- database/Read.php
+++ database/Read.php
@@ -32,10 +32,6 @@ class Read
      * @var
      */
     private $terms;
-    /**
-     * @var
-     */
-    private $result;
 
     /**
      * @var \PDOStatement
@@ -43,6 +39,28 @@ class Read
     private $select;
 
     private $query;
+
+    /**
+     * @var
+     */
+    private $limit;
+
+    /**
+     * @var
+     */
+    private $offset;
+
+    private $page;
+    private $link;
+    private $rows;
+    private $maxLinks;
+    private $first;
+    private $last;
+
+    /**
+     * @var
+     */
+    private $result;
 
 
     /**
@@ -69,7 +87,7 @@ class Read
 
     public function query(string $query, string $parse = null)
     {
-        if ($parse){
+        if ($parse) {
             parse_str($parse, $this->statement);
 
         }
@@ -78,7 +96,7 @@ class Read
         $this->read = DB::connect()->prepare($this->query);
         $this->read->execute($this->statement);
         $this->result = $this->read->fetchAll(\PDO::FETCH_OBJ);
-
+        return $this;
 
 
     }
@@ -88,11 +106,10 @@ class Read
         $this->table = (string)$table;
 
 
-
         $this->read = DB::connect()->prepare("SELECT * FROM {$this->table}");
         $this->read->execute();
         return $this->result = $this->read->fetchAll(\PDO::FETCH_OBJ);
-
+        return $this;
 
     }
 
@@ -118,11 +135,33 @@ class Read
         } catch (\PDOException $e) {
             echo $e->getMessage() . " in " . $e->getFile();
         }
+
+        return $this;
     }
 
     public function getRowCount()
     {
         return $this->select->rowCount();
+        return $this;
+    }
+
+    public function page(int $pag = null, int $limit)
+    {
+        $this->page = ($pag ? $pag : 1);
+        $this->limit = $limit;
+        $this->offset = ($this->page * $this->limit) - $this->limit;
+
+        try {
+
+            $this->read = DB::connect()->prepare("SELECT * FROM {$this->table} LIMIT {$this->limit} OFFSET {$this->offset}");
+            $this->read->execute();
+            $this->result = $this->read->fetchAll(\PDO::FETCH_OBJ);
+        }catch (\PDOException $e){
+            echo $e->getMessage() . " in " . $e->getFile();
+
+        }
+
+
     }
 
 
